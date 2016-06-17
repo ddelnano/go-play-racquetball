3c64338aed97152bd653f7175db0d2b044256612
Domenic Del Nano <ddelnano@gmail.com>
2016-06-16 21:49:23 -0400

    Fix bash error

--- travis.sh
+++ travis.sh
@@ -2,7 +2,7 @@
 
 go test ./... -short -v
 
-if [ "$TRAVIS_TAG" ] then;
+if [ "$TRAVIS_TAG" ]; then
     echo "This is when a docker image would be built"
 else
     echo "This build should not build a docker image"
